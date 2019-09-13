3ff6b656b318b0d78c9d9c44a7584f5364b24d61
Andrew Maguire <andrewm4894@gmail.com>
2019-09-13 15:53:04 +0000

    adding tests for package

---
+++ packages/netdata_api_utils/netdata_api_utils/tests/test_utils.py
@@ -0,0 +1,15 @@
+from netdata_api_utils.utils import api, get_metrics
+
+def test_api_has_mirrored_hosts():
+    # test api pulls back at least some mirrored_hosts
+    assert len(api()['mirrored_hosts'])>=1
+
+    
+def test_get_metrics_has_rows():
+    # test you get some rows from calling get_metrics()
+    assert get_metrics().shape[0]>=1
+    
+
+def test_get_metrics_has_expected_cols():
+    # test you get expected number of cols
+    assert get_metrics().shape[1]==4
